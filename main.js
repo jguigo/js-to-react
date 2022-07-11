d3c029d48afc24c7fdd97e33acf9bccc0ea54de0
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-11 19:42:28 -0300

    Add the ScoreBoard on main

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import "./src/styles/settings/colors.css"; //primeiro carregando as cores
 import "./src/styles/generic/reset.css"; //depois os generics
 import "./src/styles/elements/base.css"; //por fim os elementos
 import BoardGame from "./src/objects/BoardGame";
-import PlayerName from "./src/components/PlayerName";
+import ScoreBoard from "./src/objects/ScoreBoard";
 
 //declaração de variáveis utilizando '$' significa que aquela variável guarda alguma referencia da tela!
 const $root = document.querySelector("#root");
@@ -10,8 +10,7 @@ const $root = document.querySelector("#root");
 $root.insertAdjacentHTML(
    "beforeend",
    `
-      ${PlayerName("Player 1")}
-      ${PlayerName("Player 2")}
+      ${ScoreBoard()}      
       ${BoardGame(6)}
    `
 );
