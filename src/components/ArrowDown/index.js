740ef616118332ebc8fe86c4e78f6ac0688576dd
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-13 15:37:42 -0300

    Add CSS on ArrowDown

--- src/components/ArrowDown/index.js
+++ src/components/ArrowDown/index.js
@@ -1,8 +1,11 @@
 import './style.css'
 
-function ArrowDown() {
+function ArrowDown(currentPlayer = 1) {
    return /*html*/ `
-      <img src="images/IconArrowDown.svg" alt="ícone de uma seta para baixo">
+      <img class="arrow-down"
+           data-currentPlayer="${currentPlayer}"
+           src="images/IconArrowDown.svg" 
+           alt="ícone de uma seta para baixo">
    `
 }
 
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -7,7 +7,7 @@ import './style.css'
 function ScoreBoard() {
    return /*html*/`
       <header class="score-board">
-      ${ArrowDown()}
+      ${ArrowDown(2)}
       ${PlayerName("Player 1")}
       ${PlayerScore(3)}
       ${VsPlayer()}
