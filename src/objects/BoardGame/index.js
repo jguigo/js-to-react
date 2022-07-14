0a5a1ffde92c91fff2b025c1828a81eccdb01131
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-13 21:47:21 -0300

    Swap player when flip two cards

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -3,13 +3,38 @@ import CardFrontBack from "../../components/CardFrontBack";
 import cards from "./data";
 
 function BoardGame() {
+   const flipAndHideCards = ($cardsActive) => {
+      $cardsActive.forEach((card) => card.classList.remove("-active"));
+   };
 
-   const htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.altIcon))
-   const $htmlCards = htmlCardsList.join("")
+   const swapPlayer = () => {
+      const $arrowDown = document.querySelector(".arrow-down");
+      const currentPlayer = $arrowDown.getAttribute("data-currentplayer");
+
+      $arrowDown.setAttribute("data-currentplayer", currentPlayer == 1 ? 2 : 1);
+   };
+
+   window.boardGame = {};
+   window.boardGame.handleClick = (event) => {
+      const $boardGame = document.querySelector(".board-game");
+      const $cardsActive = $boardGame.querySelectorAll(
+         ".card-front-back.-active"
+      );
+
+      if ($cardsActive.length === 2) {
+         setTimeout(() => {
+            flipAndHideCards($cardsActive);
+            swapPlayer();
+         }, 1000);
+      }
+   };
+
+   const htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.altIcon));
+   const $htmlCards = htmlCardsList.join("");
 
    return /*html*/ `
-   <section class="board-game">
-   ${$htmlCards}
+   <section class="board-game" onClick="boardGame.handleClick(event)">
+      ${$htmlCards}
    </section>`;
 }
 
